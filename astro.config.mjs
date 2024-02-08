415ccc17d88f984278339f0055153ca41c540732
3x10RaiseTo8 <abhishekcy17@gmail.com>
2024-02-09 03:44:21 +0530

    Add rss functionality

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,5 @@
 import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://blog-astro4.netlify.app/',
+});
